b02f0ccb84b1e0f2a5bce623d77081d27fd1eafb
Adam Mohammed Muzammil <mm592678@gmail.com>
2025-03-07 14:42:37 +0530

    CHanges in layout files

--- app/converter/layout.jsx
+++ app/converter/layout.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { getSession } from "../../utils/getSession";
 import Navbar from "../../components/dashboard/Navbar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/shared/app-sidebar";
 
 const TrendingLayout = async ({ children }) => {
-  const details = await getSession();
   return (
     <SidebarProvider>
       <AppSidebar />
